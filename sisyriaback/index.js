6c1f8b58a66d9050d004f1642f159d42c3fd283c
AnwarRamo <anwarramo2333@gamil.com>
2025-06-11 14:23:16 +0300

    Initial commit of backend project with Docker setup

--- sisyriaback/index.js
+++ sisyriaback/index.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/authRouter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const __dirname = path.resolve();
 
 // CORS Configuration - IMPORTANT for frontend to connect
