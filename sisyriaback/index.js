19125b3c7b30cd575b554b3a3f7d34c45f4a187d
AnwarRamo <anwarramo2333@gamil.com>
2025-06-11 15:49:02 +0300

    Initial commit

--- sisyriaback/index.js
+++ sisyriaback/index.js
@@ -13,11 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const __dirname = path.resolve();
 
-// CORS Configuration - IMPORTANT for frontend to connect
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000", 
-    credentials: true,
+  origin: process.env.CLIENT_URL || "https://sisyriafinly.netlify.app", 
+  credentials: true,
 }));
+
 
 // Middlewares
 app.use(express.json()); 
